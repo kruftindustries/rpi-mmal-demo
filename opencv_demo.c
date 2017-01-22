bb323692f3b47d5ab579211f0e31f3482d07a325
kruftindustries <kd0ohr@gmail.com>
2017-01-22 05:08:28 -0600

    Update opencv_demo.c
    #includes for opencv changed from *.hpp to *_c.h since this is a c project and those are compiled as c++.

--- opencv_demo.c
+++ opencv_demo.c
@@ -9,9 +9,9 @@
 #include <stdlib.h>
 
 #include <opencv2/core/core_c.h>
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect_c.h>
+#include <opencv2/highgui/highgui_c.h>
+#include <opencv2/imgproc/imgproc_c.h>
 
 #include "bcm_host.h"
 #include "interface/vcos/vcos.h"
